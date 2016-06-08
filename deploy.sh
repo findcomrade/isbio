1fca0653ff95032615a849fb7108f05b03a33ddd
Clément Fiere <clement.fiere@helsinki.fi>
2016-06-08 18:29:50 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -14,15 +14,15 @@ version=$(<.version)
 echo ${version}>.version
 
 if [ "$HOST_NAME" = breeze.giu.fi ]; then
-	echo "${BLUE}ON PROD (${HOST_NAME})${END_C}"
+	echo -e "${BLUE}ON PROD (${HOST_NAME})${END_C}"
 	ENVIRONMENT='production'
-	echo "${CYAN}git status${END_C}"
+	echo -e "${CYAN}git status${END_C}"
 	git status
-	echo "${CYAN}git checkout${END_C}"
+	echo -e "${CYAN}git checkout${END_C}"
 	git checkout
-	if [ $(git checkout|tail -n1) != "$CLEAN_GIT" ];
+	if [ "$(git checkout|tail -n1)" != "$CLEAN_GIT" ];
 	then
-		echo "${RED}WARNING:${END_C} Non committed local changes will be discarded."
+		echo -e "${RED}WARNING:${END_C} Non committed local changes will be discarded."
 		read -p "Are you sure you want to proceed (y/n) ?" -n 1 -r
 		echo # move to a new line
 		if [[ $REPLY =~ ^[Yy]$ ]];
@@ -34,23 +34,23 @@ if [ "$HOST_NAME" = breeze.giu.fi ]; then
 			exit 0
 		fi
     fi
-    if [ $(git checkout|tail -n1) != "$CLEAN_GIT" ];
+    if [ "$(git checkout|tail -n1)" != "$CLEAN_GIT" ];
 	then
-		echo "${RED}ERROR:${END_C} ambiguous git status, please check git co manually !"
+		echo -e "${RED}ERROR:${END_C} ambiguous git status, please check git co manually !"
 		exit 1
     fi
-	echo "${CYAN}git pull${END_C}"
+	echo -e "${CYAN}git pull${END_C}"
 	git pull
 else
 	if [ "$HOST_NAME" = breeze-dev.giu.fi ]; then
-		echo "${BLUE}ON DEV (${HOST_NAME})${END_C}"
+		echo -e "${BLUE}ON DEV (${HOST_NAME})${END_C}"
 		ENVIRONMENT='development'
 	fi
 fi
 
 NEW_SELF=`md5sum deploy.sh`
 if [ "$NEW_SELF" != "$SELF" ]; then
-	echo "${BLUE}Deploy script has been changed${END_C}, re-starting..."
+	echo -e "${BLUE}Deploy script has been changed${END_C}, re-starting..."
 	./deploy.sh &
 	exit $?
 fi
@@ -58,7 +58,7 @@ fi
 ACCESS_TOKEN=00f2bf2c84ce40aa96842622c6ffe97d
 LOCAL_USERNAME=`whoami`
 REVISION=`git log -n 1 --pretty=format:"%H"`
-echo "${BLUE}Registering deploy version "${version}", git "${REVISION}" ...${END_C}"
+echo -e "${BLUE}Registering deploy version "${version}", git "${REVISION}" ...${END_C}"
 
 curl https://api.rollbar.com/api/1/deploy/ \
   -F access_token=${ACCESS_TOKEN} \
@@ -69,5 +69,5 @@ curl https://api.rollbar.com/api/1/deploy/ \
 echo
 killall autorun.sh > /dev/null 2>&1
 killall autorun_sub.sh > /dev/null 2>&1
-echo "Reloading BREEZE..."
+echo -e "${BLUE}Reloading BREEZE...${END_C}"
 ./autorun.sh &
