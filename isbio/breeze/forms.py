3e85d825c6cf49e6b4ef3051feffd11ae919a869
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-10 19:48:40 +0300

    form validation issuer

--- isbio/breeze/forms.py
+++ isbio/breeze/forms.py
@@ -748,7 +748,7 @@ class NewScriptDialog(forms.Form):
 	category = forms.ModelChoiceField(
 		queryset=breeze.models.ScriptCategories.objects.all(),
 		empty_label="Please select one ...",
-		to_field_name='name'
+		to_field_name='category'
 	)
 
 	inline = forms.CharField(
