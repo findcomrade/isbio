5cbf7a200ce5e77c5682c3d3ec20fc7127777bed
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-14 19:54:00 +0300

    added passing env variable to docker run, and docker interface passing azure_key

--- isbio/breeze/docker_interface.py
+++ isbio/breeze/docker_interface.py
@@ -684,7 +684,7 @@ class DockerInterface(ComputeInterface):
 	def send_job(self):
 		self._set_global_status(self.js.PREPARE_RUN) # TODO change
 		if self._upload_assembly():
-			env = { 'AZURE_KEY': self.storage_backend.ACCOUNT_KEY } # passing the blob storage secret key to the cont
+			env = { 'AZURE_KEY': self._job_storage.ACCOUNT_KEY } # passing the blob storage secret key to the cont
 			self.my_run = DockerRun(self.config_container, self.config_cmd % self.run_id, self.my_volume)
 			self._attach_event_manager()
 			if self._run():
