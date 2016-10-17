58d08a3a894304d0f4bfa74da97feec430ff4836
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-17 19:50:13 +0300

    added api app to centralize all json interface

--- isbio/api/urls.py
+++ isbio/api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from . import views as api_views
 
 urlpatterns = [
-	url(r'^?$', api_views.api_home, name='api.home'),
+	url(r'^$', api_views.api_home, name='api.home'),
 	url(r'^legacy/$', include('api.views_legacy')),
 	url(r'^v1/$', include('api.urls_v1')),
 ]
