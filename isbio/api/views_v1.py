b6cc67e50d1670516ed60389bd726e5cd066a8d4
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-17 23:02:52 +0300

    verified POST HMAC SIG

--- isbio/api/views_v1.py
+++ isbio/api/views_v1.py
@@ -16,6 +16,7 @@ import time
 # from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
 
 CT_JSON = 'application/json'
+CT_TEXT = 'text/plain'
 empty_dict = dict()
 VERSION = '1.0'
 
@@ -41,38 +42,76 @@ def get_response(data=empty_dict, result=200, message=''):
 
 
 # clem 17/10/2016
-def hmac(data, key):
-	import hmac
+class MyWSGIReq(WSGIRequest):
 	import hashlib
+	import hmac as _hmac_lib
+	H_SIG_HEADER = 'HTTP_X_HUB_SIGNATURE'
+	H_REQ_METHOD = 'REQUEST_METHOD'
+	H_C_T = 'CONTENT_TYPE'
+	H_HOST = 'HTTP_HOST'
+	H_REMOTE_IP = 'REMOTE_ADDR'
+	H_USER_AGENT = 'HTTP_USER_AGENT'
 	
-	# digest_maker = hmac.new(bytearray(key, 'utf-8'), bytearray(data, 'utf-8'), hashlib.sha1)
-	digest_maker = hmac.new(key, data, hashlib.sha1) #  data.encode('utf-8')
-	# digest_maker.update(data)
-	return digest_maker.hexdigest()
+	def __init__(self, request):
+		assert isinstance(request, WSGIRequest)
+		super(MyWSGIReq, self).__init__(request.environ)
+	
+	def hmac(self, key, algorithm=hashlib.sha1):
+		return self._hmac_lib.new(key, self.body, algorithm).hexdigest() # data.encode('utf-8')
+
+	def get_meta(self, key, default=''):
+		return str(self.META.get(key, default))
+
+	def get_meta_low(self, key, default=''):
+		return self.get_meta(key, default).lower()
+	
+	def get_meta_up(self, key, default=''):
+		return self.get_meta(key, default).upper()
+
+	@property
+	def signature(self):
+		return self.get_meta(self.H_SIG_HEADER)
+	
+	@property
+	def has_sig(self):
+		return self.signature.startswith('sha1')
+	
+	@property
+	def is_post_hook(self):
+		return self.get_meta_up(self.H_REQ_METHOD) == 'POST'
+	
+	@property
+	def is_json_post(self):
+		return self.is_post_hook and self.get_meta_low(self.H_C_T) == CT_JSON
+
+	@property
+	def client_id(self):
+		return '%s (%s) / %s' %\
+			(self.get_meta(self.H_HOST), self.get_meta(self.H_REMOTE_IP), self.get_meta(self.H_USER_AGENT))
+
+	def check_sig(self, key=None):
+		if self.is_json_post and self.has_sig and self.body:
+			if not key:
+				key = get_key_magic(1)
+			if self.signature.endswith(self.hmac(key)):
+				logger.info('VERIFIED sig FROM %s' % self.client_id)
+				return self.body
+			else:
+				logger.error('!! SIGNATURE MISMATCH FROM %s' % self.client_id)
+		return False
 
 
 # clem 17/10/2016
-def check_signature(request):
-	assert isinstance(request, WSGIRequest)
-	sig = str(request.META.get('HTTP_X_HUB_SIGNATURE', ''))
-	content_t = request.META.get('CONTENT_TYPE', '')
-	print content_t
-	host = request.META.get('REMOTE_HOST', '')
-	agent = request.META.get('HTTP_USER_AGENT', '')
-	this_id = '%s / %s' % (host, agent)
-	# HTTP_USER_AGENT
-	raw_body = request.body
-	#  = json.loads(request.body)
-	# payload = request.POST.get('payload', None)
-	if sig and raw_body:
-		# print('sig:', sig)
-		key = get_key_magic(1)
-		digest = hmac(raw_body, key)
-		if sig.startswith('sha1') and sig.endswith(digest):
-			logger.info('VERIFIED SIG FROM %s' % this_id)
-			return json.loads(raw_body)
-		else:
-			logger.error('SIGNATURE MISMATCH' % this_id)
+def get_json(request_init):
+	request = MyWSGIReq(request_init)
+	key = get_key_magic(1)
+	try:
+		json_data = request.check_sig(key)
+		if json_data:
+			return json.loads(json_data)
+	except Exception as e:
+		logger.exception(str(e))
+
 	return None
 	
 
@@ -98,20 +137,20 @@ def hook(_):
 # clem 17/10/2016
 @csrf_exempt
 def reload_sys(request):
-	payload = check_signature(request)
+	payload = get_json(request)
 	if payload:
 		# TODO filter json request
 		import subprocess
 		subprocess.Popen('sleep 2 && git pull', shell=True)
 		return get_response(payload, message='ok')
 		
-	return get_response(result=400, )
+	return get_response(result=400)
 	
 	
 # clem 17/10/2016
 @csrf_exempt
 def git_hook(request):
-	payload = check_signature(request)
+	payload = get_json(request)
 	if payload:
 		# TODO filter json request
 		return get_response(payload, message='ok')
