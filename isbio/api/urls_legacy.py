3d277e3920888847c54eea8aef61c2f80d906678
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-17 19:52:38 +0300

    added api app to centralize all json interface

--- isbio/api/urls_legacy.py
+++ isbio/api/urls_legacy.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views_legacy as legacy
 
 urlpatterns = [
-	url(r'^script-editor/get-content/(?P<content>[^/-]+)(?P<iid>-\d+)??$', legacy.show_templates,
+	url(r'^script-editor/get-content/(?P<content>[^/-]+)(?P<iid>-\d+)?/?$', legacy.show_templates,
 		name='legacy.show_templates'),
 ]
