0294d7ce0c1ac7d51e6a6014adbed8119aa83bdb
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-19 12:43:50 +0300

    fixed issue with python's Process lib AttributeError exception of line 79

--- isbio/breeze/process.py
+++ isbio/breeze/process.py
@@ -1,8 +1,10 @@
-from multiprocessing import Process
+from multiprocessing import Process as ProcessOriginal
 import os
 import sys
 import itertools
-__author__ = 'clem'
+__author__ = 'clem' # fork of python process module
+
+__all__ = ['Process', 'current_process', 'active_children']
 
 try:
 	ORIGINAL_DIR = os.path.abspath(os.getcwd())
@@ -10,7 +12,34 @@ except OSError:
 	ORIGINAL_DIR = None
 
 
-class MyProcess(Process, object):
+def current_process():
+	'''
+	Return process object representing the current process
+	'''
+	return _current_process
+
+
+def active_children():
+	'''
+	Return list of process objects corresponding to live child processes
+	'''
+	_cleanup()
+	return list(_current_process._children)
+
+
+#
+#
+#
+
+def _cleanup():
+	# check for processes which have finished
+	for p in list(_current_process._children):
+		
+		if hasattr(p, '_popen') and hasattr(p._popen, 'poll') and p._popen.poll() is not None:
+			_current_process._children.discard(p)
+
+
+class MyProcess(ProcessOriginal):
 	def _bootstrap(self):
 		from multiprocessing import util
 		global _current_process
@@ -51,3 +80,51 @@ class MyProcess(Process, object):
 
 		util.info('process exiting with exitcode %d' % exitcode)
 		return exitcode
+
+
+#
+# We subclass bytes to avoid accidental transmission of auth keys over network
+#
+
+class AuthenticationString(bytes):
+	def __reduce__(self):
+		from multiprocessing.forking import Popen
+		if not Popen.thread_is_spawning():
+			raise TypeError(
+				'Pickling an AuthenticationString object is '
+				'disallowed for security reasons'
+			)
+		return AuthenticationString, (bytes(self),)
+
+
+#
+# Create object representing the main process
+#
+
+class _MainProcess(MyProcess):
+	def __init__(self):
+		self._identity = ()
+		self._daemonic = False
+		self._name = 'MainProcess'
+		self._parent_pid = None
+		self._popen = None
+		self._counter = itertools.count(1)
+		self._children = set()
+		self._authkey = AuthenticationString(os.urandom(32))
+		self._tempdir = None
+
+
+_current_process = _MainProcess()
+del _MainProcess
+
+#
+# Give names to some return codes
+#
+
+_exitcode_to_name = { }
+
+for name, signum in signal.__dict__.items():
+	if name[:3] == 'SIG' and '_' not in name:
+		_exitcode_to_name[-signum] = name
+		
+Process = MyProcess
