d5b4d11030ec0794d2d70a02c8b4665a8daec163
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-19 12:08:09 +0300

    added logging of AUTH procedure, and clearing the session profile upon failure

--- isbio/hello_auth/views.py
+++ isbio/hello_auth/views.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.contrib import auth
 from django.shortcuts import redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from utilz import logger
 from django.contrib.auth.decorators import login_required
 # import os
 import json
@@ -47,23 +49,25 @@ def process_login(request):
 			user_info = requests.get(user_url).json()
 			
 			user = auth.authenticate(**user_info)
+			assert isinstance(user, User)
 			# We're saving all user information into the session
 			request.session['profile'] = user_info
 			
 			if user and user.is_active:
 				auth.login(request, user)
+				logger.info('AUTH success for %s (%s)' % (user.username, user.email))
 			else:
+				request.session['profile'] = None
+				logger.warning('AUTH denied for %s (%s)' % (user.username, user.email))
 				return HttpResponse(status=403)
-				# return redirect(settings.AUTH0_SUCCESS_URL)
 		else:
-			print token_info
-			
+			print(token_info)
 			if token_info['error'] == 'access_denied':
+				logger.warning('AUTH failure [%s]' % str(token_info))
 				return HttpResponse(status=503)
-	
 	else:
-		print 'unsupported auth type :'
-		print request.GET.__dict__
+		logger.warning('AUTH invalid GET[%s] POST[%s]' % (request.GET.__dict__, request.POST.__dict__))
+		print ('unsupported auth type :\n', request.GET.__dict__, request.POST.__dict__)
 	
 	return index(request)
 
