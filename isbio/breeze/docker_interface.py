fceedc9901a63438c841ed633363b2fbe141b7cb
Clément Fiere <clement.fiere@helsinki.fi>
2016-10-20 17:44:29 +0300

    added a system check per enabled target, and compute interface are now telling if they are online or not 8

--- isbio/breeze/docker_interface.py
+++ isbio/breeze/docker_interface.py
@@ -820,7 +820,7 @@ def initiator(compute_target, *_):
 	with a_lock:
 		if use_caching:
 			key_id = compute_target.runnable.short_id if hasattr(compute_target.runnable, 'short_id') else ''
-			key = '%s:%s' % ('DockerInterface', compute_target.runnable.short_id)
+			key = '%s:%s' % ('DockerInterface', key_id)
 			cached = ObjectCache.get(key)
 			if not cached:
 				ObjectCache.add(new_if(), key, expire_after)
