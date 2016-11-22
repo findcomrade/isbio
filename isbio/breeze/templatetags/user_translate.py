483ac08bddac506056fdbfef61deebce76730be9
Clément Fiere <clement.fiere@helsinki.fi>
2016-11-22 14:48:48 +0200

    added dev title for breeze on dev mode

--- isbio/breeze/templatetags/user_translate.py
+++ isbio/breeze/templatetags/user_translate.py
@@ -1,9 +1,14 @@
-__author__ = 'clem'
-
 from django import template
+from django.conf import settings
+__author__ = 'clem'
 
 register = template.Library()
 
 @register.simple_tag
 def fullname(user):
-	return "%s %s" % (user.first_name, user.last_name)
+	return "%s %s" % (user.first_name, user.last_name)\
+
+
+@register.simple_tag
+def run_mode_name():
+	return 'dev' if settings.DEV_MODE else ''
