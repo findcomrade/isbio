6f40ef764c873fa4cb867cf0adce263dfe372402
Clément Fiere <clement.fiere@helsinki.fi>
2016-11-22 15:55:31 +0200

    fixing context processor warning with render and dict

--- isbio/breeze/non_db_objects.py
+++ isbio/breeze/non_db_objects.py
@@ -1062,6 +1062,6 @@ if VERSION[0] >= 1 and VERSION[1] >= 9:
 	class RequestContext(dict):
 		def __init__(self, _, dict_=None, *args, **kwargs):
 			super(RequestContext, self).__init__()
-			self = dict_
+			self = dict_ or dict()
 else:
 	RequestContext = ReqCont
