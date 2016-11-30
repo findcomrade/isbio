4459cb6d44a123d94deb3700aa811607e7d12bea
Clément Fiere <clement.fiere@helsinki.fi>
2016-11-30 20:13:15 +0200

    fixed ALLOWED HOSTS

--- isbio/isbio/config/mode_pharma.py
+++ isbio/isbio/config/mode_pharma.py
@@ -1,7 +1,10 @@
 from isbio.config.CAS import *
+from isbio.settings import PH_DOMAINS
 from new_sge import *
 
 TEMPLATE_DEBUG = False
 DEBUG = False
 SHINY_MODE = 'remote'
 SHINY_LOCAL_ENABLE = False
+
+ALLOWED_HOSTS = PH_DOMAINS + [CAS_SERVER_IP]
