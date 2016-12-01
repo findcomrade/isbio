62a9661fe28fa43fd07034f896544cd1d158686a
Clément Fiere <clement.fiere@helsinki.fi>
2016-12-01 17:05:49 +0200

    restores default ModelBackend.user_can_authenticate to CASBackend 2

--- isbio/my_django/cas_ng_custom.py
+++ isbio/my_django/cas_ng_custom.py
@@ -1,9 +1,18 @@
 """CAS authentication backend override"""
 from django_cas_ng.backends import CASBackend as org_CASBackend
-from django.contrib.auth.backends import ModelBackend
+# from django.contrib.auth.backends import ModelBackend
 
 
 class CASBackend(org_CASBackend):
 	"""CAS authentication backend override"""
-	user_can_authenticate = ModelBackend.user_can_authenticate
+	
+	def user_can_authenticate(self, user):
+		"""
+		Reject users with is_active=False. Custom user models that don't have
+		that attribute are allowed.
+		note : duplicated code from django.contrib.auth.backends.ModelBackend
+			since "user_can_authenticate = ModelBackend.user_can_authenticate" was not working
+		"""
+		is_active = getattr(user, 'is_active', None)
+		return is_active or is_active is None
 
