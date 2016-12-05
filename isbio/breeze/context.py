7c655310985f0ab90c3200f5751fe7c7e55ece18
Clément Fiere <clement.fiere@helsinki.fi>
2016-12-05 16:44:26 +0200

    added git branch and commit display in nav bar for dev mode

--- isbio/breeze/context.py
+++ isbio/breeze/context.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from breeze.utilities import git_get_branch, git_get_commit
+
+
 def user_context(request):
     if request.user.is_authenticated():
         is_admin = request.user.groups.filter(name='GEEKS')
@@ -8,7 +12,11 @@ def user_context(request):
 
     return {
         'is_local_admin': is_admin,
-        'is_authenticated': is_auth
+        'is_authenticated': is_auth,
+        'run_mode': settings.RUN_MODE,
+        'dev_mode': settings.DEV_MODE,
+        'git_branch': git_get_branch(),
+        'git_commit': git_get_commit(),
     }
 
 
